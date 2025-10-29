bb1c11abacf5a118f363b1fbce774bcd7cc9c672
Rajveer <csb21075@tezu.ac.in>
2025-10-29 19:25:26 +0530

    Add Apis

--- src/main/java/com/lean/lean/repository/LeanEntityRepository.java
+++ src/main/java/com/lean/lean/repository/LeanEntityRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 @Repository
 public interface LeanEntityRepository extends JpaRepository<LeanEntity, Long> {
     Optional<LeanEntity> findByEntityId(String entityId);
+    Optional<LeanEntity> findByUserId(String userId);
 }
